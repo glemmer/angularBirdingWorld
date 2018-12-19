866aa4cf69f307585d661da1e974ee0afa38aaa3
glemmer <gert.lemmer@gmail.com>
2018-12-20 09:53:03 +1000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { BirdsComponent } from './birds/birds.component';
+import { LocationsComponent } from './locations/locations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    BirdsComponent,
+    LocationsComponent
   ],
   imports: [
     BrowserModule,
